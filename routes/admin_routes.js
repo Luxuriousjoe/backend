594b4f37ffc1d027133d2e2068330e01305a5c55
Luxuriousjoe <156549315+Luxuriousjoe@users.noreply.github.com>
2026-04-06 21:20:40 +0100

    coo

--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin_controller');
 const { adminMiddleware } = require('../middleware/auth_middleware');
+const updateUserPermissionsHandler =
+  adminController.updateUserPermissions || adminController.updateUser;
 
 router.get('/users', adminMiddleware, adminController.getAllUsers);
 router.post('/users', adminMiddleware, adminController.createUser);
 router.post('/admins', adminMiddleware, adminController.createAdmin);
 router.patch('/users/:id/toggle', adminMiddleware, adminController.toggleUser);
 router.put('/users/:id', adminMiddleware, adminController.updateUser);
-router.patch('/users/:id/permissions', adminMiddleware, adminController.updateUserPermissions);
+router.patch('/users/:id/permissions', adminMiddleware, updateUserPermissionsHandler);
 router.delete('/users/:id', adminMiddleware, adminController.deleteUser);
 router.put('/users/:id/password', adminMiddleware, adminController.changeUserPassword);
 router.get('/logs', adminMiddleware, adminController.getLogs);
